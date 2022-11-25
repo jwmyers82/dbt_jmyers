86a81b9b6b2c1919289f100baee3f9cf93d644c2
Jeremy Myers <jwmyers82@gmail.com>
2022-11-25 16:22:51 +0000

    working

--- models/people_group_people_group.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='table') }}
-
--- people_group_people_group
-SELECT DISTINCT
-  groupName AS people_group,
-  partOf AS related_people_group
-FROM `mockecommerce-342202.bible.theography_people_groups`
